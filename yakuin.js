1c418aab6b7cb0d4265030d2a6d823463fb7b4cf
hayat0-suzuki <42994944+hayat0-suzuki@users.noreply.github.com>
2018-11-26 10:33:56 +0900

    Update yakuin.js

--- yakuin.js
+++ yakuin.js
@@ -1,1 +1,4 @@
 javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value + "[preview id=311373346 ht=20]";})()
+
+//コンパイル済み
+javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value+"[preview id=311373346 ht=20]"})();
