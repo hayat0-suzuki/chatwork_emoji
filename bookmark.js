8d752302dffeb81af9a5a87ac57ae21dbf2ae234
hayat0-suzuki <42994944+hayat0-suzuki@users.noreply.github.com>
2018-11-26 11:46:39 +0900

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,4 +1,9 @@
-javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value + "[preview id=311373346 ht=20]";})()
+javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value + "[preview id=311373346 ht=32]";})()
+javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value + "[preview id=311443514 ht=32]";})()
 
 //コンパイル済み
-javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value+"[preview id=311373346 ht=20]"})();
+
+//yakuin
+javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value+"[preview id=311373346 ht=32]"})();
+//ultra_fast_taniyama
+javascript:(function(){document.getElementById("_chatText").value=document.getElementById("_chatText").value+"[preview id=311443514 ht=32]"})();
